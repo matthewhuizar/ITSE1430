3dbf9f812c7c19de6f9d3aa575451710b2b82c90
matthewhuizar <matthew.huizar@my.tccd.edu>
2017-09-18 17:47:06 -0500

    new setup

--- Classwork/Section1/HelloWorld/HelloWorld/Program.cs
+++ Classwork/Section1/HelloWorld/HelloWorld/Program.cs
@@ -1,19 +1,16 @@
-﻿/*
- * Matthew Huizar
- * ITSE 1430
- * 9/1/2017
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1 {
-    class Program {
+namespace HelloWorld
+{
+    class Program
+    {
         static void Main( string[] args )
         {
+            Console.WriteLine("Hello World");
         }
     }
 }
