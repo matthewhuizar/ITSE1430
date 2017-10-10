1ae80691eb5b4ff0b4da7bbda992232bc477a90d
matthewhuizar <matthew.huizar@my.tccd.edu>
2017-10-09 19:06:22 -0500

    10/9

--- Classwork/Section2/Nile/Nile.Windows/Program.cs
+++ Classwork/Section2/Nile/Nile.Windows/Program.cs
@@ -12,6 +12,14 @@ namespace Nile.Windows
         [STAThread]
         static void Main()
         {
+            //String split example
+            //var csv = "Field1 | Field2 | Field3 | Field4"; //Elements to split
+            //var delimiters = new char[2];
+            //delimiters[0] = '|';
+            //delimiters[1] = ',';
+            //var tokens = csv.Split(delimiters, StringSplitOptions.RemoveEmptyEntries); //String split, array
+            //var numberOfElements = tokens.Length; 
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
